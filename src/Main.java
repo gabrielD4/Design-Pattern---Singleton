604c1764a8a2e648e2096f5f2a3efca3894a6e41
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-03-26 14:54:29 +0100

    fix logical problem with singleton

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,14 @@ public class Main {
     private static User user;
 
     public static void main(String[] args) {
-        User avatar1 = getUser();
+        User avatar1 = User.getUser();
         avatar1.printUserInfo();
 
-        User avatar2 = getUser();
+        User avatar2 = User.getUser();
         avatar2.setName("Linda");
         avatar2.setAge(28);
         avatar2.printUserInfo();
+        avatar1.printUserInfo();
+    }
 
-    }
-    public static User getUser() {
-        if(user == null) {
-            user = new User();
-        }
-        return user;
-    }
 }
--- src/User.java
+++ src/User.java
@@ -4,9 +4,18 @@ public class User {
 
     private int age;
 
-    public User() {
+    private static User instance;
+
+    private User() {
         this.name = "Gabriel";
         this.age = 31;
+    }
+
+    public static User getUser() {
+        if(instance == null) {
+            instance = new User();
+        }
+        return instance;
     }
 
     public void printUserInfo() {
